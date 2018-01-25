6745e0e293da55c6676d662ab69eb444dd898ea3
Henry <henrypye@outlook.com>
2018-01-25 14:46:35 +0000

    Started adding login functions / queries

--- routes/routes.js
+++ routes/routes.js
@@ -6,43 +6,51 @@ module.exports = function (app) {
 		res.send('Howdy');
 	});
 
+	app.post('/signup', function(req, res) {
+		queries.SignUp(req.body).then(function(result) {
+			res.send(result);
+		}).catch(function(err) {
+			res.send(err);
+		});
+	});	
+
 	app.get('/displayall', function (req, res) {
 		queries.DisplayAll().then(function (result) {
 			res.send(result);
-		}).catch(function (result) {
-			res.send(result);
+		}).catch(function (err) {
+			res.send(err);
 		});
 	});
 
 	app.get('/find/:id', function (req, res) {
 		queries.FindByDeviceId(req.params.id).then(function (result) {
 			res.json(result);
-		}).catch(function (result) {
-			res.send(result);
+		}).catch(function (err) {
+			res.send(err);
 		});
 	});
 
 	app.post('/insert', function (req, res) {		
 		queries.Insert(req.body).then(function (result) {
 			res.json(result);
-		}).catch(function (result) {
-			res.send(result);
+		}).catch(function (err) {
+			res.send(err);
 		});
 	});
 
 	app.post('/deleteall', function (req, res) {
 		queries.DropAll().then(function (result) {
 			res.json(result);
-		}).catch(function (result) {
-			res.send(result);
+		}).catch(function (err) {
+			res.send(err);
 		});
 	});
 
 	app.post('/delete/:id', function (req, res) {
 		queries.DropByDeviceId(req.params.id).then(function (result) {
 			res.json(result);
-		}).catch(function (result) {
-			res.send(result);
+		}).catch(function (err) {
+			res.send(err);
 		});
 	});
 };

2b9c951a143612f1de143b31eab481e28d912239
Henry Pye <henrypye@outlook.com>
2018-05-13 09:30:48 +0100

    Routes separated out for readability

--- app.js
+++ app.js
@@ -1,39 +1,31 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var port = 3000;
-var position = require('./models/position');
-var authentication = require('./routes/authentication');
-var insertion = require('./routes/insertion');
-var find = require('./routes/find');
-var deletion = require('./routes/deletion');
-var cronOps = require('./actions/cron-ops');
+var express = require('express'),
+	cors = require('cors'),
+	compression = require('compression'),
+	bodyParser = require('body-parser'),
+	config = require('./config'),
+	checkLastLogCron = require('./actions/cron-ops');
 
-app.options("/*", function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Request-With');
-	res.sendStatus(200);
-});
+var app = express();
+
+	var AuthRoute = require('./Routes/Authentication'),
+		InsertRoute = require('./Routes/Insertion'),
+		FindRoute = require('./Routes/Find'),
+		DeleteRoute = require('./Routes/Delete');
+
+app.use(cors());
+app.use(compression());
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-
 app.use(bodyParser.json());
 
-app.use('/api/find', find);
-app.use('/api/insert', insertion);
-app.use('/api/delete', deletion);
-app.use('/api/auth', authentication);
+app.use('/api/find', FindRoute);
+app.use('/api/insert', InsertRoute);
+app.use('/api/delete', DeleteRoute);
+app.use('/api/auth', AuthRoute);
 
-app.get('/', function(req, res, next) {
-	res.send('hey');
-});
-
-require('./routes/routes')(app);
-
-var cron = new cronOps();
+var cron = new checkLastLogCron();
 
 app.listen(port);
 
